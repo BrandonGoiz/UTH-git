d0b24b7bb02ca85c6ceb640559d296db5efd60e2
Abraham <abrahamtrinidad015@gmail.com>
2023-10-19 08:46:58 -0600

    first commit

---
+++ application/logs/log-2023-10-19.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-10-19 00:57:17 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 01:05:17 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\message.php 49
+ERROR - 2023-10-19 01:05:17 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\message.php 49
+ERROR - 2023-10-19 01:05:17 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\message.php 51
+ERROR - 2023-10-19 01:10:24 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 01:31:32 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 01:38:31 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 01:40:46 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 01:40:51 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 01:42:06 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 06:54:18 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 06:56:26 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 07:11:33 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 07:19:13 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 07:30:18 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 07:31:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 07:33:45 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
+ERROR - 2023-10-19 07:40:24 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\app-cursos\application\views\backend\admin\users.php 52
